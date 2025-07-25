e7cf1c7b9e41fcb57193ea659afc11bbe857c7c7
danielazecheru <dcz218@stern.nyu.edu>
2025-07-24 20:09:39 -0400

    Update rock-paper-scissors.py

---
+++ app/rock-paper-scissors.py
@@ -0,0 +1,15 @@
+# This is my rock paper scissors game 
+
+print("Welcome to my game!")
+
+player_choice = input("Please select an option from ('rock', 'paper', 'scissors'):")
+
+import random
+
+computer_choice = random.choice("rock", "paper", "scissors")
+
+print("Computer choice:", computer_choice)
+
+# to do: some code to determine winner
+
+print("Winner: todo")
