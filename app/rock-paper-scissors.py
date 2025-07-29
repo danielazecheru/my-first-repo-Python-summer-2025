8293d68b704831c6aab81c48b59c9fc866f71b74
danielazecheru <dcz218@stern.nyu.edu>
2025-07-29 19:18:15 -0400

    refactored logic in a function that can be tested

--- app/rock-paper-scissors.py
+++ app/rock-paper-scissors.py
@@ -12,17 +12,21 @@ computer_choice = random.choice(choice)
 
 print("Computer choice:", computer_choice)
 
-if player_choice=='rock' and computer_choice=='paper':
-    print("Sorry, computer won this round. Please try again!")
-elif player_choice=='rock' and computer_choice=='scissors':
-    print("Congratulations! You won this round!")
-elif player_choice=='paper' and computer_choice=='rock':
-    print("Congratulations! You won this round!")
-elif player_choice=='paper' and computer_choice=='scissors':
-    print("Sorry, computer won this round. Please try again!")
-elif player_choice=='scissors' and computer_choice=='paper':
-    print("Congratulations! You won this round!")
-elif player_choice=='scissors' and computer_choice=='rock':
-    print("Sorry, computer won this round. Please try again!")
-else:
-    print("This round was a tie. Please try again!")
+def determine_winner(player_choice, computer_choice):
+    if player_choice=='rock' and computer_choice=='paper':
+        result = "Sorry, computer won this round. Please try again!"
+    elif player_choice=='rock' and computer_choice=='scissors':
+        result = "Congratulations! You won this round!"
+    elif player_choice=='paper' and computer_choice=='rock':
+        result = "Congratulations! You won this round!"
+    elif player_choice=='paper' and computer_choice=='scissors':
+        result = "Sorry, computer won this round. Please try again!"
+    elif player_choice=='scissors' and computer_choice=='paper':
+        result = "Congratulations! You won this round!"
+    elif player_choice=='scissors' and computer_choice=='rock':
+        result = "Sorry, computer won this round. Please try again!"
+    else:
+        result = "This round was a tie. Please try again!"
+    return result
+
+print(determine_winner(player_choice, computer_choice))
